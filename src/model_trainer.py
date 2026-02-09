41d42fd2659c860b0b4af946771ee301dfbec105
BigM19 <nmatedan@gmail.com>
2026-02-09 19:46:16 +0100

    Created cross validation script

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -25,11 +25,26 @@ class ModelTrainer:
     def cross_validation(self,X, y, params):
         """Runs CV by fitting Preprocessor and Model independently per fold."""
         fold_scores = []
-        best_iters = []
 
         for train_idx, test_idx in self.tscv.split(X):
             X_train, X_test = X[train_idx], X[test_idx]
             y_train, y_test = y[train_idx], y[test_idx]
 
-            model = XGBClassifier(**self.base_params, **params
-
+            # Independent Preprocessor per fold
+            preprocessor = Preprocessor()
+            X_train_pca = preprocessor.fit_transform(X_train)
+            X_test_pca = preprocessor.transform(X_test)
+            
+            # Train model on PCA-transformed data
+            model = XGBClassifier(**self.base_params, **params, n_estimators=5000)
+            model.fit(
+                X_train_pca, y_train,
+                eval_set=[(X_test_pca, y_test)],
+                early_stopping_rounds=50,
+                verbose=False
+            )
+            
+            score = average_precision_score(y_test, model.predict_proba(X_test_pca)[:, 1])
+            fold_scores.append(score)
+            
+            return float(np.mean(fold_scores))
