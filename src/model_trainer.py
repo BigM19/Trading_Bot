1cb4c4b6e9c07f4f69c4981677502eb1d14abaa3
BigM19 <nmatedan@gmail.com>
2026-02-09 19:55:11 +0100

    Added function for parameter sampling and experiment running

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -7,6 +7,12 @@ from sklearn.model_selection import TimeSeriesSplit, ParameterSampler
 from sklearn.metrics import average_precision_score, roc_auc_score
 from src.preprocessing import Preprocessor
 from config import MODEL_DIR, MODEL_PATH
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 class ModelTrainer:
     def __init__(self, n_splits=5, n_iter=50):
@@ -48,3 +54,24 @@ class ModelTrainer:
             fold_scores.append(score)
             
             return float(np.mean(fold_scores))
+
+    def run_experiment(self, X, y, param_grid):
+        """Logs every hyperparameter combination as a child run in MLflow."""
+        sampler = ParameterSampler(param_grid, n_iter=self.n_iter, random_state=69)
+        best_score = -np.inf
+        best_params = None
+        
+        for i, params in enumerate(sampler):
+            # Log each hyperparameter combination as a child run
+            with mlflow.start_run(run_name=f"XGB_CV_{i}"):
+                mean_aucpr = self.cross_validation(X, y, params)
+                
+                mlflow.log_params(params)
+                mlflow.log_metric("mean_aupr", mean_aucpr)
+                
+                if mean_aucpr > best_score:
+                    best_score = mean_aucpr
+                    best_params = params
+                    
+            logging.info(f"Completed run {i+1}/{self.n_iter} with AUPR: {mean_aucpr:.4f}")
+        return best_params
